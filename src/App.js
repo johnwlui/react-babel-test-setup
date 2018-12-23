31be1f9f704cba6e253934e13e3f95e811726d83
johnwlui <johnwlui@hotmail.com>
2018-12-24 00:18:18 +1100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+
+export const doIncrement = (prevState) => ({
+  counter: prevState.counter + 1,
+});
+
+export const doDecrement = (prevState) => ({
+  counter: prevState.counter - 1,
+});
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      counter: 0,
+      asyncCounters: null,
+    };
+
+    this.onIncrement = this.onIncrement.bind(this);
+    this.onDecrement = this.onDecrement.bind(this);
+  }
+
+  componentDidMount() {
+    axios.get('http://mydomain/counter')
+      .then(counter => this.setState({ asyncCounters: counter }))
+      .catch(error => console.log(error));
+  }
+
+  onIncrement() {
+    this.setState(doIncrement);
+  }
+
+  onDecrement() {
+    this.setState(doDecrement);
+  }
+
   render() {
+    const { counter } = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>My Counter</h1>
+        <Counter counter={counter} />
+
+        <button
+          type="button"
+          onClick={this.onIncrement}
+        >
+          Increment
+        </button>
+
+        <button
+          type="button"
+          onClick={this.onDecrement}
+        >
+          Decrement
+        </button>
       </div>
     );
   }
 }
+
+export const Counter = ({ counter }) =>
+  <p>{counter}</p>
 
 export default App;
